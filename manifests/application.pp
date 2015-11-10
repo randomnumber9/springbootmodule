0cabc690b4c990fa35d6ccf40f1cc01424b54b3f
John Cook <john.cook@lifeway.com>
2015-11-10 14:55:06 -0600

    stripping whitespace from var

--- manifests/application.pp
+++ manifests/application.pp
@@ -35,7 +35,7 @@ define springbootmodule::application(
         mode    => 0775
       } ->
 
-      exec { "/usr/bin/wget -N strip(${no_whitespace_source})":
+      exec { "/usr/bin/wget -N ${no_whitespace_source}":
         alias   => "springbootlatest${title}",
         cwd     => "${path}/cache/${app_name}",
       }
